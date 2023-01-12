95780ebfb17b44d66af1bf037450d7f2839a10ca
Yasar Sulaiman <yasar.sulaiman@supportlogic.com>
2023-01-12 22:21:59 +0530

    docker changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Docker, Jenkins and Kubernetes and AWS
         </a>
       </header>
     </div>
